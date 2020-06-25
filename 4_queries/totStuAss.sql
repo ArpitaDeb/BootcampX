e59f1d62987901f3c9fba4738f463fc5271b1c12
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-25 20:05:36 +0000

    Querying The Database

--- 4_queries/totStuAss.sql
+++ 4_queries/totStuAss.sql
@@ -1,5 +1,10 @@
-SELECT count(assistance_requests.*) as total_assistances, students.name as name
-FROM  assistance_requests
-JOIN students ON student_id = students.id
-WHERE name = 'Elliot Dickinson'
-GROUP BY students.name;
+SELECT
+  count(assistance_requests.*) as total_assistances,
+  students.name as name
+FROM
+  assistance_requests
+  JOIN students ON student_id = students.id
+WHERE
+  name = 'Elliot Dickinson'
+GROUP BY
+  students.name;
---
+++ bootcamp_app/students.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+ORDER BY id ASC
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+  })
+});
+
